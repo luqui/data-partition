8db187d0433d0d7f1f18190670be1e29f14aff80
Luke Palmer <luqui@clozure.com>
2013-01-14 02:16:46 -0700

    Documentation

--- Data/Partition.hs
+++ Data/Partition.hs
@@ -1,16 +1,48 @@
-module Data.Parititon where
+--------------------------------------------------------------------------
+-- |
+-- Module               : Data.Parition
+-- Copyright            : (c) Luke Palmer, 2013
+-- License              : BSD3
+--
+-- Maintainer           : Luke Palmer <lrpalmer@gmail.com>
+-- Stability            : experimental
+-- Portability          : portable
+--
+-- Disjoint set data structure -- @Partition a@ maintains a collection of
+-- disjoint sets of type @a@, with the ability to find which set a particular
+-- item belongs to and the ability to merge any two such sets into one.
+---------------------------------------------------------------------------
+
+module Data.Parititon 
+    ( Partition, discrete, empty, join, find )
+ where
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Maybe (fromMaybe)
 
+-- | A Partition of @a@: represents a collection of disjoint sets of @a@ whose
+-- union includes every element of @a@.  Semantics: @[[Partition a]] = P(P(a))@
+-- where @P@ is the power set operation.
 data Partition a 
     = Partition { forwardMap :: Map.Map a a, backwardMap :: Map.Map a (Set.Set a) }
-    deriving (Show)
+    deriving (Eq, Ord)  -- Since the representative is always the least element,
+                        -- we have a canonical representation and Eq is meaningful.
+                        -- Ord may not mean anything, but at least there some 
+                        -- computable total ordering on Partitions, and that is helpful
+                        -- sometimes.
 
+-- | A partition in which every element of @a@ is in its own set.  Semantics:
+-- @[[discrete]] = { { x } | x in a }@
+discrete :: Partition a
+discrete = Partition Map.empty Map.empty
+
+-- | Synonym for "discrete".
 empty :: Partition a
-empty = Partition Map.empty Map.empty
+empty = discrete
 
+-- | @join x y@ merges the two sets containing @x@ and @y@ into a single set.  Semantics:
+-- @[[join x y p]] = (p `minus` find x `minus` find y) `union` { find x `union` find y }@
 join :: (Ord a) => a -> a -> Partition a -> Partition a
 join x y p = case compare x' y' of
                  LT -> go x' y'
@@ -29,12 +61,16 @@ join x y p = case compare x' y' of
         where
         otherSrc = reprFind p other
 
+-- | @find p x@ finds the set that the element @x@ is associated with.  Semantics: 
+-- @[[find p x]] = the unique s in p such that x in s@.
 find :: (Ord a) => Partition a -> a -> Set.Set a
 find p = reprFind p . repr p
 
+-- Find the representative element for an element.
 repr :: (Ord a) => Partition a -> a -> a
 repr p x = fromMaybe x (Map.lookup x (forwardMap p))
 
+-- Find the set that x is in given that x is already a representative element.
 reprFind :: (Ord a) => Partition a -> a -> Set.Set a
 reprFind p x = fromMaybe (Set.singleton x) (Map.lookup x (backwardMap p))
 
